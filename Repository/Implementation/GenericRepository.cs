1610d1e6564becb1f88e9e5f0320b049adbf7de1
Benjamin Friske <benjaminfriske@gmail.com>
2020-06-09 10:05:23 -0500

    Style cop fixes for GenericRepository.

--- Repository/Implementation/GenericRepository.cs
+++ Repository/Implementation/GenericRepository.cs
@@ -6,50 +6,79 @@ using Learning.DesignPatterns.Infrastructure.Contexts;
 
 namespace Learning.DesignPatterns.Infrastructure.Repository.Implementation
 {
-    class GenericRepository<T>
-        : IRepository<T> where T : class
+    /// <summary>
+    /// Generic repository.
+    /// </summary>
+    /// <typeparam name="T">Repository type.</typeparam>
+    public class GenericRepository<T> : IRepository<T>
+        where T : class
     {
-        protected BenjaminFriskeContext context;
+        private BenjaminFriskeContext context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericRepository{T}"/> class.
+        /// </summary>
+        /// <param name="context">Entity framework default context.</param>
         public GenericRepository(BenjaminFriskeContext context)
         {
             this.context = context;
         }
 
+        /// <summary>
+        /// Virtual add method to be overidden by repsoitory that inherits this class.
+        /// </summary>
+        /// <param name="entity">Any entity type.</param>
+        /// <returns>Entity added value.</returns>
         public virtual T Add(T entity)
         {
-            return context
-                .Add(entity)
-                .Entity;
+            return this.context.Add(entity).Entity;
         }
 
+        /// <summary>
+        /// Virtual Find method to be overidden by repsoitory that inherits this class.
+        /// </summary>
+        /// <param name="predicate">Method that defines a set of criteria and determines whether the specified object meets those criteria.</param>
+        /// <returns>Found entity.</returns>
         public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
         {
-            return context.Set<T>()
-                .AsQueryable()
-                .Where(predicate).ToList();
+            return this.context.Set<T>().AsQueryable().Where(predicate).ToList();
         }
 
+        /// <summary>
+        /// Virtual get method to be overidden by repsoitory that inherits this class.
+        /// </summary>
+        /// <param name="id">Guid of the entity.</param>
+        /// <returns>Entity found by guid.</returns>
         public virtual T Get(Guid id)
         {
-            return context.Find<T>(id);
+            return this.context.Find<T>(id);
         }
 
+        /// <summary>
+        /// Virtual all method to be overidden by repsoitory that inherits this class.
+        /// </summary>
+        /// <returns>Set of entities found.</returns>
         public virtual IEnumerable<T> All()
         {
-            return context.Set<T>()
-                .ToList();
+            return this.context.Set<T>().ToList();
         }
 
+        /// <summary>
+        /// Virtual update method to be overidden by repsoitory that inherits this class.
+        /// </summary>
+        /// <param name="entity">Entity needing updated.</param>
+        /// <returns>Updated entity.</returns>
         public virtual T Update(T entity)
         {
-            return context.Update(entity)
-                .Entity;
+            return this.context.Update(entity).Entity;
         }
 
+        /// <summary>
+        /// Virtual save method to be overidden by repsoitory that inherits this class.
+        /// </summary>
         public void SaveChanges()
         {
-            context.SaveChanges();
+            this.context.SaveChanges();
         }
     }
 }
